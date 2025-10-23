19255f0f014af5d4ce20f7f4d707b926268fc0e4
Adriana <adrhuamaniperez@crackthecode.la>
2025-10-23 16:43:48 -0500

    carpeta raiz

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/pastel-a/", 
+  base: "/dulzura-pastel/", // 👈 asegúrate que este nombre sea igual al del repo
 })
